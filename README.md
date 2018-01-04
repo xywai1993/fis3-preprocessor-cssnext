81160daa4a3905468bf14158af1d03f1cef22d7d
xywai1993 <www163ep@163.com>
2018-01-04 16:50:49 +0800

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # fis3-preprocessor-cssnext
 fis3插件 postcss  cssnext  
+
+```javascript
+fis.match('css/postcss.scss',{
+    rExt: '.css', // from .scss to .css
+    parser: fis.plugin('node-sass',{
+        sourceMap:true,
+        outFile:'css'
+    }),
+    preprocessor:fis.plugin('cssnext')
+})
+```
---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+const postcss = require('postcss');
+const cssnext = require('postcss-cssnext');
+
+
+module.exports = function(content, file, settings){
+    console.log('content',content);
+    // postcss([cssnext({browsers:settings.Browserslist})]).process(content,{inline:false}).then(data=>{
+    //     console.log(data.css);
+    // })
+    const map = settings.sourceMap?{inline:false}:false;
+    // 源地图设置 https://github.com/postcss/postcss/blob/master/docs/source-maps.md
+    return postcss([cssnext({browsers:settings.Browserslist})]).process(content,{to:file.release,map:map}).css ;
+}
+
+module.exports.defaultOptions = {
+    sourceMap: true,
+    Browserslist:[
+       "> 1%, last 2 versions, Firefox ESR, Opera 12.1" 
+    ]
+  };
